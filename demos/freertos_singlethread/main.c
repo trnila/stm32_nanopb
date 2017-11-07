7a1d67c1d765890032beca06d5959c5b294fda3a
Daniel Trnka <daniel.trnka@gmail.com>
2017-11-07 15:38:47 +0000

    Sending serialized data to uart

--- demos/freertos_singlethread/main.c
+++ demos/freertos_singlethread/main.c
@@ -65,28 +65,72 @@ void uart_1_task(void *param) {
 		if(input >= 'a' && input <= 'z') {
 			input -= 'a' - 'A';
 		}
+		send_byte_1('x');
 		send_byte_1(input);
 	}
 }
 
+const int STATE_TYPE = 0;
+const int STATE_SIZE = 1;
+const int STATE_CONTENT = 2;
+void uart_processor(void* param) {
+	static char input[10];
+	static char inp;
+	static char pos = 0;
+	static int val;
+	static int type, size;
+	static char state = 0;
+	for(;;) {
+		xQueueReceive(rxQueue, (void*) &inp, portMAX_DELAY);
+
+		if(state == STATE_TYPE || state == STATE_SIZE) {
+			input[pos] = inp;
+			val |= inp << (pos * 8);
+			pos = (pos + 1) % 4;
+			if(pos == 0) {
+				printf("RCV: %d %x %x %x %x\r\n", val, input[0], input[1], input[2], input[3]);
+
+				if(state == STATE_TYPE) {
+					state = STATE_SIZE;
+					type = val;
+				} else if (state == STATE_SIZE) {
+					state = STATE_CONTENT;
+					size = val;
+				}
+				val = 0;
+				pos = 0;
+			}
+		} else if (state == STATE_CONTENT) {
+			input[pos] = inp;
+			pos++;
+			if(pos == size) {
+				printf("content: %s\r\n", input);
+				state = STATE_TYPE;
+				val = 0;
+				pos = 0;
+			}
+			
+		} else {
+			printf("fail %d\r\n", "");
+		}
+	}
+}
+
+
 void USART1_IRQHandler(void) {
-	static char input;
-	static portBASE_TYPE wake;
+	static char inp;
+	static int wake;
 
 	if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET) {
-		input = USART_ReceiveData(USART1);
-		if(rxQueue != NULL) {
-			xQueueSendFromISR(rxQueue, &input, &wake);
-		} else {
-			panic("rxQueue NULL");
-		}
+		inp = USART_ReceiveData(USART1);
+		xQueueSendFromISR(rxQueue, &inp, &wake);
 	} else {
-		panic("Unhandled uart event");
+		panic("err");
 	}
 }
 
 void init() {
-	rxQueue = xQueueCreate(10, sizeof(char));
+	rxQueue = xQueueCreate(255, sizeof(char));
 	if(rxQueue == NULL) {
 		//TODO: uart not available yet!
 		panic("Could not allocate queue");
@@ -112,7 +156,8 @@ int main(void)
 	xTaskCreate( led_flash_task, ( signed portCHAR * ) "LED Flash", 512 /* stack size */, NULL, tskIDLE_PRIORITY + 5, NULL );
 	xTaskCreate( led_flash2_task, ( signed portCHAR * ) "LED Flash", 512 /* stack size */, NULL, tskIDLE_PRIORITY + 5, NULL );
 	//	xTaskCreate(uart_task, "UART", 128, NULL, tskIDLE_PRIORITY + 5, NULL);
-	xTaskCreate(uart_1_task, "UART", 128, NULL, tskIDLE_PRIORITY + 5, NULL);
+	//xTaskCreate(uart_1_task, "UART", 128, NULL, tskIDLE_PRIORITY + 5, NULL);
+	xTaskCreate(uart_processor, "UART", 128, NULL, tskIDLE_PRIORITY + 5, NULL);
 
 	/* Start running the task. */
 	vTaskStartScheduler();
